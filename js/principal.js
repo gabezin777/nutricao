47e44852dac8d492d5b8f063bcd5ad0284b0e5ea
Gabriel D. <gabriel.cunha20@escola.pr.gov.br>
2025-10-07 09:15:02 -0300

    dasda

--- js/principal.js
+++ js/principal.js
@@ -1,8 +1,13 @@
- var titulo = document.querySelector("h1");
-        console.log(titulo);
-        console.log(titulo.textContent);
-        titulo.textContent = ("Aparecida Nutricionista");
- var paciente =document.querySelector("#primeiro");     
- var tdPeso =paciente.querySelector(".info-peso")     
-       console.log(paciente);as    
-       console.log (Peso);// td que tem o peso
+var titulo = document.querySelector("h1");
+console.log(titulo);
+console.log(titulo.textContent);
+titulo.textContent = ("Aparecida Nutricionista");
+var paciente = document.querySelector("#primeiro");
+var tdPeso = paciente.querySelector(".info-peso")
+
+console.log(paciente); as
+console.log(Peso);// td que tem o peso
+
+var tdAltura = document.querySelector(" .info-altura")
+var altura = tdAltura.texteConte;
+console.log(altura)
