abee434fdcb1b066c5ac3d2533c3c4868f37da83
Gabriel D. <gabriel.cunha20@escola.pr.gov.br>
2025-10-07 09:18:06 -0300

    \x\z

--- js/principal.js
+++ js/principal.js
@@ -6,7 +6,7 @@ var paciente = document.querySelector("#primeiro");
 var tdPeso = paciente.querySelector(".info-peso")
 
 console.log(paciente); as
-console.log(Peso);// td que tem o peso
+console.log(peso);// td que tem o peso
 
 var tdAltura = document.querySelector(" .info-altura")
 var altura = tdAltura.texteConte;
