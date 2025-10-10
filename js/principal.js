e8b5fa3e3c8aa5445500e18610eecdd48abab0a0
Gabriel D. <gabriel.cunha20@escola.pr.gov.br>
2025-10-10 11:14:44 -0300

    4

--- js/principal.js
+++ js/principal.js
@@ -28,15 +28,13 @@ if (peso < 0 || peso > 1000) {
   tdImc.textContent = "Peso invalido!";
 }
 
-
 if(altura < 0||altura > 3.00){
    console.log("Altura inválida");
     var alturaEhValido = false;
     tdImc.textContent="Altura invalida!";
 }
 
-
- if(alturaEhValida && pessoEhValido){
+ if(alturaEhValida && pesoEhValido){
    var imc = peso / (altura * altura) ;
    tdImc.textContent = imc;
  }
