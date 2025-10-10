15727da476f60a31a14ccc06e892265ee85b0f33
Gabriel D. <gabriel.cunha20@escola.pr.gov.br>
2025-10-10 11:13:26 -0300

    1

--- js/principal.js
+++ js/principal.js
@@ -4,42 +4,35 @@ console.log(titulo);
 console.log(titulo.textContent);
 titulo.textContent = ("gabe");
 
-
-
-
 var paciente = document.querySelector("#primeiro-paciente");
+var tdNome = document.querySelector("info-nome");
+var nome = tdNome.textContent;
+console.log(nome);
+
 var tdPeso = paciente.querySelector(".info-peso");
 var peso = tdPeso.textContent;
-console.log(paciente);
-console.log(tdpeso);
+console.log(peso);
 
-
-
-
-var tdAltura = document.querySelector("#primeiro-paciente");
-var altura = document.querySelector(".info-altura");
+var tdAltura = document.querySelector(".info-altura");
 var altura = tdAltura.textContent;
 console.log(altura);
 
 
-
-
 //vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
 var pesoehValido = true;
- var alturaehValido = true;
-
+var alturaehValido = true;
 
 if (peso < 0 || peso > 1000) {
   console.log("peso inválido"); 
   var pesoehValido = false;
-  tdlmc.textContent = "Peso ivalido!";
+  tdImc.textContent = "Peso invalido!";
 }
 
 
 if(altura < 0||altura > 3.00){
    console.log("Altura inválida");
-    var alturaehValido = false;
-    tdlmc.textContent="Altura invalida!";
+    var alturaEhValido = false;
+    tdImc.textContent="Altura invalida!";
 }
 
 
