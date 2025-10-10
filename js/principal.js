13d52e1bebd62a3b195f29bf45bfabc7d7255e3f
Gabriel D. <gabriel.cunha20@escola.pr.gov.br>
2025-10-10 10:47:33 -0300

    asdfas

--- js/principal.js
+++ js/principal.js
@@ -1,13 +1,50 @@
+//console.log(document.querySelector("h1"));
 var titulo = document.querySelector("h1");
 console.log(titulo);
 console.log(titulo.textContent);
-titulo.textContent = ("Aparecida Nutricionista");
-var paciente = document.querySelector("#primeiro");
-var tdPeso = paciente.querySelector(".info-peso")
+titulo.textContent = ("Pedro nutritations");
 
-console.log(paciente); as
-console.log(peso);// td que tem o peso
 
-var tdAltura = document.querySelector(" .info-altura")
-var altura = tdAltura.texteConte;
-console.log(altura)
+
+
+var paciente = document.querySelector("#primeiro-paciente");
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+console.log(paciente);
+console.log(tdPeso);
+
+
+
+
+var tdAltura = document.querySelector("#primeiro-paciente");
+var altura = document.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+console.log(altura);
+
+
+
+
+//vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+var pesoehValido = true;
+ var alturaehValido = true;
+
+
+if (peso < 0 || peso > 1000) {
+  console.log("peso inválido"); 
+  var pesoehValido = false;
+  tdlmc.textContent = "Peso ivalido!";
+}
+
+
+if(altura < 0||altura > 3.00){
+   console.log("Altura inválida");
+    var alturaehValido = false;
+    tdlmc.textContent="Altura invalida!";
+}
+
+
+ if(alturaEhValida && pessoEhValido){
+   var imc = peso / (altura * altura) ;
+   tdImc.textContent = imc;
+ }
+
