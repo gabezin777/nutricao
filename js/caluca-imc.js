6715a5ddf3e0275427ebd7facde2669b47145bbb
Gabriel D. <gabriel.cunha20@escola.pr.gov.br>
2025-11-04 10:04:45 -0300

    as

---
+++ js/caluca-imc.js
@@ -0,0 +1,75 @@
+//console.log(document.querySelector("h1"));
+var titulo = document.querySelector("h1");
+console.log(titulo);
+console.log(titulo.textContent);
+titulo.textContent = ("Nicolas nutritations");
+
+
+var pacientes = document.querySelectorAll(".paciente");
+
+
+for(var i = 0; i < pacientes.length; i++) {
+
+
+var paciente = pacientes[i];
+
+
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+
+
+var tdImc = paciente.querySelector(".info-imc");
+
+
+var pesoEhValido = true;
+var alturaEhValido = true;
+
+
+if (peso <= 0 || peso >= 1000){
+console.log("Peso inválido!");
+pesoEhvalido = false;
+tdImc.textContent = "Peso inválido!";
+paciente.classList.add("paciente-invalido");
+}
+
+
+if (altura <= 0 || altura >= 3.00){
+console.log("Altura inválida!");
+alturaEhValido = false;
+tdImc.textContent = "Altura inválida!";
+paciente.classList.add("paciente-invalido");
+}
+
+
+if (alturaEhValido && pesoEhValido){
+var imc = CalculaImc(peso, altura);
+tdImc.textContent = imc.toFixed(2);
+}
+}
+
+
+function calculaImc (peso,altura){
+   var imc = 0;
+   var imc = peso / (altura* altura);
+   return imc.toFixed(2);
+}
+
+
+
+
+titulo.addEventListener("click", mostraMensagem);
+function mostraMensagem(){
+alert("Olá eu fui clicado.")
+}
+
+
+
+
+
+
+
+
